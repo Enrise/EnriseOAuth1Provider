a38d27448712165aa6013887879f5931b194fc6b
Dolf Schimmel (Freeaqingme) <dolfschimmel@gmail.com>
2012-11-11 17:49:20 +0100

    Initial Commit

---
+++ Module.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Enrise OAuth1Provider  (http://enrise.com/)
+ *
+ * @link      https://github.com/Enrise/EnriseOAuth1Provider for the canonical source repository
+ * @copyright Copyright (c) 2012 Dolf Schimmel - Freeaqingme (dolfschimmel@gmail.com)
+ * @copyright Copyright (c) 2012 Enrise (www.enrise.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace EnriseOAuth1Provider;
+
+require_once __DIR__ . '/src/Enrise/OAuth1/Module.php';
+
+use Enrise\OAuth1\Provider\Module as EnriseOAuth1Provider;
+
+class Module extends EnriseOAuth1Provider
+{
+
+}
---
+++ src/Enrise/OAuth1/Provider/Module.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Enrise OAuth1Provider  (http://enrise.com/)
+ *
+ * @link      https://github.com/Enrise/EnriseOAuth1Provider for the canonical source repository
+ * @copyright Copyright (c) 2012 Dolf Schimmel - Freeaqingme (dolfschimmel@gmail.com)
+ * @copyright Copyright (c) 2012 Enrise (www.enrise.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace  Enrise\OAuth1\Provider;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+
+
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ =>  __DIR__ . '/',
+                ),
+            ),
+        );
+    }
+}
